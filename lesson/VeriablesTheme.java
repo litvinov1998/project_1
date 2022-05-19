3c6540aeecc5745918db027bead0d7eeab3fa6e1
Ivan <105059533+litvinov1998@users.noreply.github.com>
2022-05-19 15:28:25 +0300

    Update VeriablesTheme.java

--- lesson/VeriablesTheme.java
+++ lesson/VeriablesTheme.java
@@ -1,7 +1,7 @@
 package lesson;
 
 public class VeriablesTheme {
-	    public static void main(String[]args) {
+        public static void main(String[]args) {
         System.out.println("1.Создание переменных и вывод их значений на консоль.");
                 byte cpu = 6;
                 short vRamMB = 6144;
@@ -12,23 +12,23 @@ public class VeriablesTheme {
                 char symbl = 127;
                 boolean namePC = true;
                 System.out.println(" cpu - " + cpu
-                                  +"\n vRamMB - " + vRamMB
-                                  +"\n ramMB - " + ramMB
-                                  +"\n gpuMHz - " + gpuMHz
-                                  +"\n cpuMHz - " + cpuMHz
-                                  +"\n ccdGB - " + ccdGB
-                                  +"\n symbl - " + symbl
-                                  +"\n namePC - " + namePC);
+                        +"\n vRamMB - " + vRamMB
+                        +"\n ramMB - " + ramMB
+                        +"\n gpuMHz - " + gpuMHz
+                        +"\n cpuMHz - " + cpuMHz
+                        +"\n ccdGB - " + ccdGB
+                        +"\n symbl - " + symbl
+                        +"\n namePC - " + namePC);
         System.out.println();
         System.out.println("2.Расчет стоимости товара со скидкой");
-                int costProduct = 100;
-                int costProduct1 = 200;
-                int price = costProduct + costProduct1;
-                int discountPercent = 11;
-                int discountValue = price * discountPercent / 100;
-                int newPrice = price - discountValue;
-                System.out.println("Discount value - " + discountValue);
-                System.out.println("New Price - " + newPrice);
+        int costProduct = 100;
+        int costProduct1 = 200;
+        int price = costProduct + costProduct1;
+        int discountPercent = 11;
+        int discountValue = price * discountPercent / 100;
+        int newPrice = price - discountValue;
+        System.out.println("Discount value - " + discountValue);
+        System.out.println("New Price - " + newPrice);
         System.out.println();
         System.out.println("3.Вывод на консоль слова JAVA");
                 System.out.println("   J    a  v     v  a \n"
@@ -46,9 +46,9 @@ public class VeriablesTheme {
                 long firstLong = 9223372036854775807l;
                 long minLong = ++firstLong, maxLong = --firstLong;
                 System.out.println(firstByte + " " + maxByte + " " + minByte + "\n"
-                                  + firstShort + " " + maxShort + " " + minShort + "\n"
-                                  + firstInt + " " + maxInt + " " + minInt + "\n"
-                                  + firstLong + " " + maxLong + " " + minLong);
+                        + firstShort + " " + maxShort + " " + minShort + "\n"
+                        + firstInt + " " + maxInt + " " + minInt + "\n"
+                        + firstLong + " " + maxLong + " " + minLong);
         System.out.println();
         System.out.println("5.Перестановка значения переменных.");
                 float a = 3.3f;
@@ -66,10 +66,10 @@ public class VeriablesTheme {
 
                 char code = 34, code1 = 38, code2 = 64, charCode = 94, charCode1 = 96;
                 System.out.println((int) code + "-" +code + "\n"+
-                                  (int) code1 + "-" +code1 + "\n"+
-                                  (int) code2 + "-" +code2 + "\n"+
-                                  (int) charCode + "-" +charCode + "\n" +
-                                  (int) charCode1 + "-" +charCode1);
+                        (int) code1 + "-" +code1 + "\n"+
+                        (int) code2 + "-" +code2 + "\n"+
+                        (int) charCode + "-" +charCode + "\n" +
+                        (int) charCode1 + "-" +charCode1);
         System.out.println();
         System.out.println("7.Произведение и сумма цифр числа");
                 int srcNum = 345;
@@ -83,12 +83,12 @@ public class VeriablesTheme {
         System.out.println("8.Вывод на консоль ASCII-арт Дюка");
                 char space = 32, oParenthesis = 40, cParenthesis = 41, slash = 47, backslash = 92, underscore = 95;
                 System.out.println("" + space + space + space + space + slash + backslash + "\n"+ space + 
-                                   "" + space + space + slash + space + space + backslash + "\n" +
-                                   "" + space + space + slash + underscore + oParenthesis + space +
-                                   "" + cParenthesis + backslash + "\n" + space + slash + space + 
-                                   "" + space + space + space + space + space + backslash + "\n" +
-                                   "" + slash + underscore + underscore + underscore + underscore +
-                                   "" + slash + backslash + underscore + underscore + backslash);
+                        "" + space + space + slash + space + space + backslash + "\n" +
+                        "" + space + space + slash + underscore + oParenthesis + space +
+                        "" + cParenthesis + backslash + "\n" + space + slash + space + 
+                        "" + space + space + space + space + space + backslash + "\n" +
+                        "" + slash + underscore + underscore + underscore + underscore +
+                        "" + slash + backslash + underscore + underscore + backslash);
         System.out.println();
         System.out.println("9.Отображение количества сотен, десятков и единиц числа");
                 int Number = 123;
@@ -96,10 +96,10 @@ public class VeriablesTheme {
                 int Dozens = 123 / 10 % 10;
                 int Units = 123 % 10;
                 System.out.println(Number + " содержит: " + Hundreds + " сотню" + "\n"
-                                   + Number + " содержит: " + Dozens + " десятка" + "\n"
-                                   + Number + " содержит: " + Units + " единицы");
+                        + Number + " содержит: " + Dozens + " десятка" + "\n"
+                        + Number + " содержит: " + Units + " единицы");
         System.out.println();
-        System.out.println("10.Преобразование секунд"); 
+        System.out.println("10.Преобразование секунд");
                 int srcSeconds = 86399;
                 int hours = srcSeconds/3600;
                 int min = (srcSeconds % 3600) / 60;
